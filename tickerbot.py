71601a2c4c6c16de20c18df7cf59335fc328754e
wreck <wreck@jobmachine.org>
2021-01-07 17:11:09 -0800

    include basic crypto exchange price data

--- tickerbot.py
+++ tickerbot.py
@@ -12,6 +12,7 @@ import json
 class Config(BaseProxyConfig):
     def do_update(self, helper: ConfigUpdateHelper) -> None:
         helper.copy("rapidapiKey")
+        helper.copy("coinapiioKey")
         helper.copy("stocktrigger")
         helper.copy("cryptotrigger")
 
@@ -68,6 +69,45 @@ class TickerBot(Plugin):
                             {arrow}{openPercentDiff}</font> from market open @ ${str(pruned_json['regularMarketOpen'])}",
                     f"<b>52 Week High:</b> ${str(pruned_json['fiftyTwoWeekHigh'])}",
                     f"<b>52 Week Low:</b> ${str(pruned_json['fiftyTwoWeekLow'])}"
+                ]
+            )
+        
+        await evt.respond(prettyMessage, allow_html=True)
+
+
+
+
+    @command.new(name=lambda self: self.config["cryptotrigger"],
+            help="Look up exchange rate of a currency pair. You must supply a pair, e.g. BTC/USD or ADA/ETH, otherwise USD will be used by default.")
+    @command.argument("exch_pair", pass_raw=True, required=True)
+    async def handler(self, evt: MessageEvent, exch_pair: str) -> None:
+        await evt.mark_read()
+
+        exch_pair = exch_pair.upper()
+        if '/' not in exch_pair:
+            exch_pair = exch_pair + '/USD'
+
+        url = f"https://rest.coinapi.io/v1/exchangerate/{exch_pair}"
+        headers = {
+            'X-CoinAPI-Key': self.config["coinapiioKey"],
+            }
+        
+        try:
+            response = await self.http.get(url, headers=headers)
+            resp_json = await response.json()
+        except Exception as e:
+            await evt.respond(f"request failed: {e.message}")
+            return None
+        try:
+            exch_rate = resp_json["rate"]
+        except Exception as e:
+            await evt.respond("No results, double check that you've chosen a real currency pair")
+            self.log.exception(e)
+            return None
+
+        prettyMessage = "<br />".join(
+                [
+                    f"<b>Current exchange rate for {exch_pair}:</b> {exch_rate}" 
                 ]
             )
         
